e70d1d34d7d99ab04e44ba679c81228e4a212b24
JiangNanMax <1321064814@qq.com>
2020-03-12 15:43:48 +0800

    command-pattern in actionlistener

---
+++ src/main/java/top/jiangnanmax/chapter07/v5/IReceiver.java
@@ -0,0 +1,11 @@
+package top.jiangnanmax.chapter07.v5;
+
+/**
+ * @author jiangnan
+ * @description IReceiver
+ * @date 2020/3/12
+ **/
+
+public interface IReceiver {
+    void response();
+}
--- src/main/java/top/jiangnanmax/chapter07/v5/MyFrame.java
+++ src/main/java/top/jiangnanmax/chapter07/v5/MyFrame.java
@@ -1,7 +1,6 @@
-package top.jiangnanmax.chapter07.v4;
+package top.jiangnanmax.chapter07.v5;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,15 +8,20 @@ import java.awt.event.ActionListener;
 /**
  * @author jiangnan
  * @description MyFrame
- * @date 2020/3/10
+ * @date 2020/3/12
  **/
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener {
 
     JPanel content = new JPanel();
 
-    ActionListener a1 = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
+    class FlowBtn extends JButton implements IReceiver {
+
+        public FlowBtn(String name) {
+            super(name);
+        }
+
+        public void response() {
             content.removeAll();
 
             content.setLayout(new FlowLayout()); // 流式布局管理器
@@ -28,10 +32,15 @@ public class MyFrame extends JFrame {
             content.add(new JButton("5"));
             content.updateUI();
         }
-    };
+    }
 
-    ActionListener a2 = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
+    class BorderBtn extends JButton implements IReceiver {
+
+        public BorderBtn(String name) {
+            super(name);
+        }
+
+        public void response() {
             content.removeAll();
 
             content.setLayout(new BorderLayout()); // 设置方位布局管理器
@@ -42,12 +51,12 @@ public class MyFrame extends JFrame {
             content.add(new JButton("中"), BorderLayout.CENTER);
             content.updateUI();
         }
-    };
+    }
 
     public void init() {
         JPanel p = new JPanel();
-        JButton btn1 = new JButton("FlowLayout演示");
-        JButton btn2 = new JButton("BorderLayout演示");
+        JButton btn1 = new FlowBtn("FlowLayout演示");
+        JButton btn2 = new BorderBtn("BorderLayout演示");
         p.add(btn1);
         p.add(btn2);
 
@@ -55,23 +64,20 @@ public class MyFrame extends JFrame {
         add(content);
 
 
-        btn1.addActionListener(a1);
-        btn2.addActionListener(a2);
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
 
         setSize(600, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 
+    public void actionPerformed(ActionEvent e) {
+        IReceiver iReceiver = (IReceiver)e.getSource();
+        iReceiver.response();
+    }
+
     public static void main(String[] args) {
         new MyFrame().init();
     }
 }
-
-/*
-        本示例中，有效事件源是init()方法中的两个JButton对象btn1、btn2，
-    命令接受是类中定义的两个ActionListener内部匿名类对象a1、a2，那么哪
-    个是Invoke呢？命令请求者是时间监听器，由JDK自身隐藏，无需编制。
-
-    方法2将与上述示例有所不同，具体在v5中实现
- */
